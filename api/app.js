a8c416c2f4765d317e922d2d2d67bdb7ae9dd0a9
Prabhu <prabhuji2910@gmail.com>
2025-03-10 10:54:44 +0530

    bug fix

--- api/app.js
+++ api/app.js
@@ -15,7 +15,7 @@ import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 import visitRoute from "./routes/visit.route.js";
-import ratingRoute from "./routes/rating.route.js";
+// import ratingRoute from "./routes/rating.route.js";
 //import verificationRoute from "./routes/verification.route.js";
 
 
@@ -34,7 +34,7 @@ app.use("/api/posts", postRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 app.use("/api/visits", visitRoute);
-app.use("/api/rating", ratingRoute);
+// app.use("/api/rating", ratingRoute);
 //app.use("/api/verification", verificationRoute);
 
 
